7a26058aa214a50d1c38a13a56512f655048c4a1
Andreu Tuzov <andreu.tuzov@gmail.com>
2017-01-14 15:44:39 +0300

    Solved exercise 1.3

--- Chapter_1/Ex_1.3.scm
+++ Chapter_1/Ex_1.3.scm
@@ -1,4 +1,11 @@
 #lang racket
 
-(define (max-two-max-numbers a b c)
-  (
+(define (square x) (* x x))
+
+(define (sum-of-squares x y) (+ (square x)
+                                (square y)))
+
+(define (sum-of-two-max x y z)
+  (cond ((and (> x z) (> y z)) (sum-of-squares x y))
+        ((and (> y x) (> z x)) (sum-of-squares y z))
+        ((and (> x y) (> z y)) (sum-of-squares x z))))
