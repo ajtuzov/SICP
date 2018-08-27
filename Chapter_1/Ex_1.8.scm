9aadb39269e6d6b22f4e1391aba9f5a69d681dfc
Andreu Tuzov <andreu.tuzov@gmail.com>
2018-08-28 01:50:19 +0300

    Solved 1.8

--- Chapter_1/Ex_1.8.scm
+++ Chapter_1/Ex_1.8.scm
@@ -1,16 +1,24 @@
 #lang racket
 
-(define (sqrt-iter guess x)
+(define (sqrt-cube x)
+  (cube-iter 1.0 x))
+
+(define (cube-iter guess x)
   (if (good-enough? guess x)
       guess
-      (sqrt-iter (improve guess x) x)))
+      (cube-iter (improve guess x) x)))
 
 (define (good-enough? guess x)
-  (< (abs (- (square improve) x)) 0.001))
+(< (abs (- (cube guess) x)) 0.0001))
 
-(define (
+(define (cube x)
+  (* x x x))
 
+(define (square x)
+  (* x x))
 
+(define (improve guess x)
+  (/ (+ (/ x (square guess)) 
+        (* 2 guess)) 
+3))
 
-(define (sqrt x)
-  (sqrt-iter 1.0 x))
